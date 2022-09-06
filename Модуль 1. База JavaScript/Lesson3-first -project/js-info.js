3365917500e8f93cf1737e689ec8b3f91953ff92
Karina <90905660+Kbusinessk@users.noreply.github.com>
2022-09-07 00:13:45 +1000

    Modul 1 Lessons #2, #3, #4.

---
+++ Модуль 1. База JavaScript/Lesson3-first -project/js-info.js
@@ -0,0 +1,5 @@
+alert('Brendan Eich создатель языка JavaScript')
+alert('Известный прототип JavaScript был создан за 10 дней')
+alert('JavaScript  появился в 1995 году')
+alert('JavaScript - это не Java')
+alert(' Спецификация языка JavaScript называется ECMAScriptt')
